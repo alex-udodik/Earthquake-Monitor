45247c83a7735b29dad860e9cc0aa97071258e4c
alex-udodik <alex.udodik@wsu.edu>
2023-10-12 11:17:04 -0700

    Added backend initial java spring project

---
+++ backend/server/src/test/java/com/eq/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eq.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
