d68446d3aed1c0b705f5b9419fbcfee3e65bed76
alex-udodik <alex.udodik@wsu.edu>
2024-12-29 21:05:27 -0800

    Added initial data pull from api and fixed json message when receiving new earthquake events from the websocket

--- frontend/client/lib/main.dart
+++ frontend/client/lib/main.dart
@@ -1,13 +1,16 @@
-import 'package:client/src/data/models/earthquake.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
-
+import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:client/src/data/models/earthquake.dart';
 
 List<Earthquake> earthquakes = [];
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -26,109 +29,116 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
 
-    // Log the connection attempt before initiating the connection
-    print("Attempting to connect to AWS WebSocket...");
+    // Fetch initial data from the API
+    fetchInitialEarthquakeData().then((_) {
+      // After fetching, connect to the WebSocket
+      print("Attempting to connect to AWS WebSocket...");
+      final websocketUrl = dotenv.env['WEBSOCKET_URL'] ?? '';
 
-    // Connect to AWS WebSocket
-    channel = WebSocketChannel.connect(
-      Uri.parse(
-          'wss://w0s7wouqu3.execute-api.us-west-2.amazonaws.com/production/'),
-    );
+      channel = WebSocketChannel.connect(
+        Uri.parse(websocketUrl),
+      );
 
-    // Delay to ensure connection setup is active
-    Future.delayed(Duration(milliseconds: 100), () {
-      print("Connection setup should now be active.");
+      Future.delayed(const Duration(milliseconds: 100), () {
+        print("Connection setup should now be active.");
+      });
+
+      channel.stream.listen(
+        (message) {
+          print("Received new earthquake");
+          handleMessage(message);
+        },
+        onDone: () {
+          print("WebSocket connection closed.");
+        },
+        onError: (error) {
+          print("WebSocket connection error: $error");
+        },
+      );
+
+      updateMarkers(earthquakes);
     });
-
-    // Listen for incoming messages
-    channel.stream.listen(
-      (message) {
-        print(
-            "Connected to AWS WebSocket!"); // This will print upon receiving the first message
-
-        // Print the entire object received from the WebSocket
-        print("Received message: $handleMessage($message)");
-
-        // Process the received message
-        dynamic jsonData = json.decode(message);
-        if (jsonData is List<dynamic>) {
-          List<Map<String, dynamic>> jsonArray =
-              jsonData.cast<Map<String, dynamic>>();
-          earthquakes =
-              jsonArray.map((jsonMap) => Earthquake.fromJson(jsonMap)).toList();
-
-          updateMarkers(earthquakes);
-        } else {
-          print("Invalid JSON format. Expected a list.");
-        }
-      },
-      onDone: () {
-        print("WebSocket connection closed.");
-      },
-      onError: (error) {
-        print("WebSocket connection error: $error");
-      },
-    );
   }
 
-// Function to attempt JSON repair
-  String attemptJsonRepair(String message) {
-    // Remove any trailing commas and extra whitespace
-    message = message.trim().replaceAll(RegExp(r',\s*$'), '');
+  Future<void> fetchInitialEarthquakeData() async {
+    final apiUrl = dotenv.env['API_URL'] ?? '';
 
-    // Wrap it in a JSON array if it looks like it might contain multiple JSON objects
-    if (!message.startsWith("[")) {
-      message = "[$message]";
-    }
-
-    // Try to balance braces and brackets
-    int openBraces = message.split('{').length - 1;
-    int closeBraces = message.split('}').length - 1;
-    int openBrackets = message.split('[').length - 1;
-    int closeBrackets = message.split(']').length - 1;
-
-    // Add missing closing braces and brackets
-    while (closeBraces < openBraces) {
-      message += '}';
-      closeBraces++;
-    }
-    while (closeBrackets < openBrackets) {
-      message += ']';
-      closeBrackets++;
-    }
-
-    print("Repaired JSON: $message");
-    return message;
-  }
-
-  void handleMessage(String rawMessage) {
     try {
-      // Try to parse the raw message as JSON
-      var decodedMessage = jsonDecode(rawMessage);
-      print("Decoded JSON: $decodedMessage");
-    } catch (e) {
-      print("Failed to parse JSON directly. Attempting to repair...");
-      String repairedJson = attemptJsonRepair(rawMessage);
+      final response = await http.get(Uri.parse(apiUrl));
 
-      try {
-        var decodedMessage = jsonDecode(repairedJson);
-        print("Decoded repaired JSON: $decodedMessage");
-      } catch (e) {
-        print("Failed to parse even after repair. Error: $e");
+      print("Response status code: ${response.statusCode}");
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> jsonData = json.decode(response.body);
+
+        // Extract the 'body' field
+        if (jsonData['body'] is List) {
+          final List<dynamic> bodyData = jsonData['body'];
+
+          // Iterate over the 'body' list and access the 'details' field
+          for (var item in bodyData) {
+            if (item['details'] != null) {
+              // Extract 'details' for each item
+              var details = item['details'];
+
+              // Convert 'details' into an Earthquake object
+              var earthquake = Earthquake.fromJson({
+                'action': details['action'],
+                'data': details['data'], // Pass the 'data' part as it is
+              });
+
+              earthquakes.add(earthquake);
+            }
+          }
+        } else {
+          print("Error: 'body' field is not a list or is missing");
+        }
+      } else {
+        print("Failed to fetch data. Status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error fetching data: $e");
+    }
+
+    print("EQ length: ${earthquakes.length}");
+  }
+
+  // Handle WebSocket messages
+  void handleMessage(String rawMessage) {
+    final List<dynamic> bodyData = json.decode(rawMessage);
+
+    List<Earthquake> tempEarthquakes = [];
+    // Iterate over the 'body' list and access the 'details' field
+    for (var item in bodyData) {
+      if (item['details'] != null) {
+        // Extract 'details' for each item
+        var details = item['details'];
+
+        // Convert 'details' into an Earthquake object
+        var earthquake = Earthquake.fromJson({
+          'action': details['action'],
+          'data': details['data'], // Pass the 'data' part as it is
+        });
+        tempEarthquakes.add(earthquake);
       }
     }
+
+    setState(() {
+      earthquakes = tempEarthquakes;
+    });
   }
 
+  // Update markers on the map
   void updateMarkers(List<Earthquake> newEarthquakes) {
-    print("Setting state");
-    print("Currently " + newEarthquakes.length.toString() + " earthquakes.");
     setState(() {
       _markers.clear();
       for (final earthquake in newEarthquakes) {
         final marker = Marker(
           markerId: MarkerId(earthquake.data.id),
           position: LatLng(
-              earthquake.data.properties.lat, earthquake.data.properties.lon),
+            earthquake.data.properties.lat,
+            earthquake.data.properties.lon,
+          ),
           infoWindow: InfoWindow(
             title: earthquake.data.properties.flynnRegion,
             snippet: earthquake.data.properties.mag.toString(),
