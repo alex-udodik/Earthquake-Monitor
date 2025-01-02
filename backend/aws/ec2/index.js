18310aff82af2d1cf79117aeb4859838699164a3
alex-udodik <alex.udodik@wsu.edu>
2025-01-01 21:26:25 -0800

    Implemented a ping-pong feature with theaws destination websocket to keep connection alive

--- backend/aws/ec2/index.js
+++ backend/aws/ec2/index.js
@@ -5,7 +5,7 @@ const mongoUtil = require('./mongo-util');
 const dotenv = require('dotenv').config();
 const EarthquakesList = require('./earthquakes-list');
 
-const reconnectInterval = 2000; // 5 seconds delay before reconnecting
+const reconnectInterval = 2000; // 2 seconds delay before reconnecting
 const sourceUrl = 'https://www.seismicportal.eu/standing_order';
 const destinationUrl = process.env.AWS_API_GATEWAY_WEBSOCKET;
 
@@ -14,54 +14,56 @@ const client = redis.createClient({ socket: { host: process.env.AWS_LIGHTSAIL_RE
 
 let sourceSock;
 let destinationSock;
+let heartbeatInterval;
 
 const now = new Date();
 const earthquakesList = new EarthquakesList();
-
-
 
 // Connect to the source WebSocket using SockJS (SeismicPortal)
 function connectSource() {
     sourceSock = new SockJS(sourceUrl);
 
     sourceSock.onopen = () => {
-
         console.log(`${now.toISOString()} Connected to source WebSocket: ${sourceUrl}`);
     };
 
     sourceSock.onmessage = async (e) => {
-        // Parse data received from source WebSocket
-
-
         const msg = JSON.parse(e.data);
-        if (msg === undefined || msg === null || msg.data === undefined || msg.data === null || msg.data.id === undefined || msg.data.id === null) {
-            console.log(`${now.toISOString()} Earthquake data received with ID undefined. Skipping.`);
+        if (!msg || !msg.data || !msg.data.id) {
+            console.log(`${now.toISOString()} Received undefined earthquake data. Skipping.`);
             return;
         }
 
-        console.log(`${now.toISOString()} Earthquake data received with ID ${msg.data.id}`);
+        const id = msg.data.id;
+        console.log(`${now.toISOString()} Earthquake data received with ID ${id}`);
         console.log(`${now.toISOString()} Time: ${msg.data.properties.time}`);
         console.log(`${now.toISOString()} Region: ${msg.data.properties.flynn_region}`);
         console.log(`${now.toISOString()} Magnitude: ${msg.data.properties.mag}`);
 
-        const id = msg.data.id;
         const filter = { "data.id": id };
+        const result = await mongoUtil.replaceDocumentOrCreateNew(
+            "EarthquakesData",
+            "Earthquake",
+            msg,
+            filter,
+            { upsert: true }
+        );
 
-        // Replace an existing document or create a new one if it doesn't exist
-        const result = await mongoUtil.replaceDocumentOrCreateNew("EarthquakesData", "Earthquake", msg, filter, { upsert: true });
-        console.log(result.modifiedCount === 0 ? `${now.toISOString()} Earthquake data with ID {${id}} created in MongoDB.` : `${now.toISOString()} Document with ID {${id}} updated in MongoDB.`);
+        console.log(
+            result.modifiedCount === 0
+                ? `${now.toISOString()} Earthquake data with ID {${id}} created in MongoDB.`
+                : `${now.toISOString()} Document with ID {${id}} updated in MongoDB.`
+        );
 
         earthquakesList.add(id, msg);
         await setKeyValueRedis("last100earthquakes", earthquakesList.toJSONString());
 
-        let dataToSend = {
+        const dataToSend = {
             action: "sendMessage",
             source: "relay-server",
             message: earthquakesList.toJSONString()
-        }
+        };
 
-
-        // Forward the data to the destination WebSocket if connected
         if (destinationSock && destinationSock.readyState === WebSocket.OPEN) {
             destinationSock.send(JSON.stringify(dataToSend));
             console.log(`${now.toISOString()} Data forwarded to destination WebSocket`);
@@ -83,16 +85,40 @@ function connectSource() {
     };
 }
 
-// Connect to the destination WebSocket using `ws`
+// Connect to the destination WebSocket
 function connectDestination() {
     destinationSock = new WebSocket(destinationUrl);
 
+    const dataToSend = {
+        action: "ping",
+        source: "relay-server",
+        message: "ping to keep server alive"
+    };
+
     destinationSock.on('open', () => {
         console.log(`${now.toISOString()} Connected to destination WebSocket: ${destinationUrl}`);
+
+        clearInterval(heartbeatInterval);
+        heartbeatInterval = setInterval(() => {
+            if (destinationSock.readyState === WebSocket.OPEN) {
+                destinationSock.send(JSON.stringify(dataToSend));
+                console.log(`${now.toISOString()} Sent heartbeat (ping)`);
+            }
+        }, 30000); // Send every 30 seconds
+    });
+
+    destinationSock.on('message', (data) => {
+        const message = JSON.parse(data);
+        if (message.action === 'pong') {
+            console.log(`${now.toISOString()} Received message from server: `, message.message);
+        }
+
+        //TODO filter out broadcast messages from websocket that come back through here
     });
 
     destinationSock.on('close', () => {
         console.log(`${now.toISOString()} Destination WebSocket disconnected. Reconnecting...`);
+        clearInterval(heartbeatInterval);
         setTimeout(connectDestination, reconnectInterval);
     });
 
@@ -102,35 +128,35 @@ function connectDestination() {
     });
 }
 
-
 // Set a key/value pair in Redis
 async function setKeyValueRedis(key, val) {
     try {
         await client.set(key, val);
-        console.log('Key set in redis successfully');
+        console.log('Key set in Redis successfully');
     } catch (error) {
-        console.error('Error setting key/value:', error);
+        console.error('Error setting key/value in Redis:', error);
     }
 }
-
 
 // Initialize MongoDB connection
 (async () => {
     try {
-        // Connect to the Redis client
         await client.connect()
             .then(() => console.log('Connected to Redis'))
             .catch((err) => console.error('Redis connection error:', err));
+
         const mongodb = MongodbSingleton.getInstance();
         await mongodb.connect();
         const last100Earthquakes = await mongoUtil.getLastXDocuments("EarthquakesData", "Earthquake", 100);
+
         populateEarthquakesList(last100Earthquakes);
         setKeyValueRedis("last100earthquakes", earthquakesList.toJSONString());
     } catch (error) {
-        console.log(`${now.toISOString()}MongoDB connection error: `, error);
+        console.log(`${now.toISOString()} MongoDB connection error:`, error);
     }
 })();
 
+// Populate earthquakes list with initial data
 function populateEarthquakesList(documents) {
     documents.forEach((doc) => {
         delete doc._id;
@@ -138,10 +164,6 @@ function populateEarthquakesList(documents) {
     });
 }
 
-
-
 // Start both WebSocket connections
 connectSource();
 connectDestination();
-
-
